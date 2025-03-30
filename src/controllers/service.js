35b41b6563242b9c96a04594c18d3627a66f3ff8
D Anuj Kumar <anuj.as828@gmail.com>
2025-03-31 04:04:55 +0530

    backend ready

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -1,211 +1,116 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user.js");
-const Hash = require("../models/hashing.js");
-const  mongoose  = require("mongoose");
+const fs = require("fs");
+const multer = require("multer");
+const path = require("path");
 
-//This is used to update the hash in mongo so that we can retrieve all information of primary and secondary efficiently
-const hashUpdater = async (id, newId ,email, phone, session)=>{
-  await Hash.updateOne({primary:id},
-    {
-      $set:{
-        primary: newId
-      },
-      $addToSet: {
-        phone: phone, // Adds the phone directly
-        email: email  // Adds the email directly
-      }
-    }
-  ).session(session)
-}
-
-//This function is used to create new entry
-const saveDetails = async (email, phone, product, linkedId, linkPrecedence, session) => {
-    return await User.create([{email, 
-      phone, 
-      product, 
-      linkedId, 
-      linkPrecedence 
-    }], {session})
-}
-
-//This function is used to update/transfer secondary contacts
-const changeofSecondary = async (oldS, newS, additional, session) => {
-  await User.updateOne ({_id: newS._id}, {
-    $set:{
-      linkPrecedence: "primary",
+// Multer Configuration (Inside Controller)
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "uploads/",
+    filename: (req, file, cb) => {
+      // Ensure req.body is available
+      const uniqueName = req.body.id + path.extname(file.originalname);
+      cb(null, uniqueName);
     },
-      $addToSet: {
-        secondaryContacts: { $each: [...(oldS.secondaryContacts || []), additional, oldS._id].filter(Boolean) }
+  }),
+}).single("photo");
+
+const post = asyncHandler(async (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    try {
+      // Check if user with the same ID already exists
+      const existingUser = await User.findOne({ id: req.body.id });
+      if (existingUser) {
+        // Delete the uploaded file since ID is duplicate
+        fs.unlink(req.file.path, (unlinkErr) => {
+          if (unlinkErr) {
+            console.error("Error deleting file:", unlinkErr);
+          }
+        });
+
+        return res
+          .status(400)
+          .json({ error: "Duplicate ID found. Choose a different ID." });
       }
-  }).session(session)  
-  await User.updateOne({_id:oldS._id}, {
-    $set:{
-      linkPrecedence : "secondary",
-      linkedId : newS._id,
-      secondaryContacts : []
-    }
-  }).session(session)
-}
 
-//This function is used to update secondary only contacts
-const secondaryUpdate = async (primary, secondary, session) => {
-  await User.updateOne({_id: primary._id}, {
-    $addToSet:{
-      secondaryContacts: {$each : [secondary._id ]} 
-    }
-  }).session(session)
-}
-
-//Endpoint here
-const identify = asyncHandler(async (req, res) => {
-  const { email: e, phone: p, product } = req.body;
-
-  const session = await mongoose.startSession()
-
-  try {
-
-  session.startTransaction();
-
-  const pc = await User.findOne({
-    $or: [{ email: e }, { phone: p }],
-    linkPrecedence: "primary",
-  }).session(session);
-
-  const sec = await User.findOne({
-    $or: [{email: e}, {phone: p}],
-    linkPrecedence: "secondary"
-  }).session(session);
-
-  if (pc) {
-    const em = pc.email;
-    const pm = pc.phone;
-
-    if (pc.linkPrecedence === "primary" && em === e && pm === p) {
-      //If both are same then create new item and make it secondary and reference it with primary
-      const sc = await saveDetails(e, p, product, pc._id, "secondary", session)
-      
-      await secondaryUpdate(pc, sc[0], session);
-      
-      await session.commitTransaction();
-
-      return res.status(200).json({
-        primaryContactId: pc._id,
-        contactPairs: [{ email: pc.email, phone: pc.phone }],
-        secondaryContactIds: pc.secondaryContacts,
-        createdAt: pc.createdAt,
-        updatedAt: pc.updatedAt,
-        deletedAt: pc.deletedAt,
+      // Create new user with image path
+      const user = new User({
+        id: req.body.id,
+        photo: req.file.path,
       });
-    } else {
-      //If email or phone number anyone changes then primary become secondary and new item become primary.
 
-      const newContact = await saveDetails(e, p, product, null, "primary", session)
-
-      
-      await hashUpdater(pc._id, newContact[0]._id, newContact[0].email, newContact[0].phone, session)
-      
-      await changeofSecondary(pc, newContact[0], null, session);
-
-      await session.commitTransaction();
-
-      return res.status(200).json({
-        primaryContactId: newContact[0]._id,
-        contactPairs: [{ email: newContact[0].email, phone: newContact[0].phone }],
-        secondaryContactIds: [pc._id],
-        createdAt: pc.createdAt,
-        updatedAt: newContact[0].updatedAt,
-        deletedAt: pc.deletedAt,
+      //Renaming the file to the ID
+      const newFilePath = path.join(
+        "uploads",
+        `${req.body.id}${path.extname(req.file.originalname)}`
+      );
+      fs.rename(req.file.path, newFilePath, (renameErr) => {
+        if (renameErr) {
+          console.error("Error renaming file:", renameErr);
+          return res.status(500).json({ error: "Error renaming file" });
+        }
       });
+
+      await user.save();
+      res.status(201).json({ message: "Image uploaded successfully", user });
+    } catch (error) {
+      // If an error occurs, delete the uploaded file
+      fs.unlink(req.file.path, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error("Error deleting file:", unlinkErr);
+        }
+      });
+
+      res.status(500).json({ error: error.message });
     }
-  }
-  else if(sec) {
-    //Here both email and phone are different but it is present in the database with secondary so we have to treat 
-    // it as same user
-
-    let hashP = await Hash.findOne({
-      email: { $in: [sec.email] },
-      phone: { $in: [sec.phone] } 
-    }).session(session);
-
-    let oldP = await User.findById(hashP?.primary).session(session);
-    // console.log(hashP.primary,oldP,hashP)
-
-    //First case: If both phone and email matches
-    let primaryC, newE, newP, secondaryC, update;
-    if(sec.email === e && sec.phone === p)
-    {
-        const newContact = await saveDetails(e, p, product, sec._id, "secondary", session)    
-        await changeofSecondary(oldP, sec, newContact[0]._id, session)
-        await hashUpdater(oldP._id, sec._id, e, p, session);
-        
-        update = newContact[0].createdAt;
-        secondaryC = sec.secondaryContacts;
-        primaryC = sec._id;
-        newE = e;
-        newP = p;
-    }
-    else
-    {
-        //Second case: If anyone matches then that becomes primary again and old primary becomes secondary
-
-        console.log("This condition will execute here...")
-
-        const newContact = await saveDetails(e, p, product, null, "primary", session)
-
-        //Old secondary becomes primary
-        await changeofSecondary(oldP, newContact[0], sec._id, session)
-        await hashUpdater(oldP._id, newContact[0]._id, newContact[0].email, newContact[0].phone, session)
-
-        primaryC = newContact[0]._id;
-        secondaryC = newContact[0].secondaryContacts;
-        update = newContact[0].createdAt;
-        newE = newContact[0].email;
-        newP = newContact[0].phone;
-    }
-
-    await session.commitTransaction()
-    
-    res.status(201).json({
-      primaryContactId: primaryC,
-      contactPairs: [{ email: newE, phone: newP }],
-      secondaryContactIds: secondaryC,
-      createdAt: oldP.createdAt,
-      updatedAt: update,
-      deletedAt: null,
-    })
-
-  }
-  else {
-    //If the information is secondary then do the same by making it primary and other thing secondary
-    //If new information with new email id or phone number come then make all together new data
-    const nc = await saveDetails(e, p, product, null, "primary", session);
-
-    await Hash.create([{
-      primary: nc[0]._id,
-      email:[nc[0].email],
-      phone:[nc[0].phone]
-    }], {session})
-
-    await session.commitTransaction()
-
-    res.status(201).json({
-      primaryContactId: nc[0]._id,
-      contactPairs: [{ email: nc[0].email, phone: nc[0].phone }],
-      secondaryContactIds: [],
-      createdAt: nc[0].createdAt,
-      updatedAt: nc[0].updatedAt,
-      deletedAt: nc[0].deletedAt,
-    });
-  }
-
-} catch(error) {
-  console.log(error)
-  await session.abortTransaction();
-  session.endSession();
-  throw new error("Something went wrong...")
-} finally {
-  session.endSession();
-}
+  });
 });
 
-module.exports = { identify };
+const getimage = asyncHandler(async (req, res) => {
+  try {
+    const { token } = req.params;
+
+    const userData = await User.findOne({ id: token });
+    if (!userData) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const finalData = {
+      user: userData,
+      imageUrl: `${req.protocol}://${req.get("host")}/uploads/${userData.id}.${userData.photo.split(".").pop()}`,
+    };
+
+    res.status(200).json(finalData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const approval = asyncHandler(async (req, res) => {
+  try {
+    const { token } = req.params;
+
+    const userData = await User.findOne({ id: token });
+    if (!userData) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    userData.approved = true;
+    await userData.save();
+
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = { post, getimage, approval };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { identify } = require('../controllers/service');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { getimage, post, approval } = require("../controllers/service");
 const router = express.Router();
 
 const app = express();
@@ -8,6 +8,8 @@ app.use(bodyParser.json());
 
 //The end point as given in the problem statement
 
-router.post('/identify', identify);
+router.get("/getimage/:token", getimage);
+router.post("/post", post);
+router.post("/approval/:token", approval)
 
 module.exports = router;
