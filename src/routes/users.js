c4826fe9502aa2f4bcf41e8b6f0113d1b8c06bbd
6015Anurag <134766075+6015Anurag@users.noreply.github.com>
2025-03-31 11:22:16 +0530

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { getimage, post, approval } = require("../controllers/service");
-const router = express.Router();
-
-const app = express();
-app.use(bodyParser.json());
-
 //The end point as given in the problem statement
 
-router.get("/api/getimage/:token", getimage);
-router.post("/api/post", post);
-router.post("/api/approval/:token", approval)
+router.get("/getimage/:token", getimage);
+router.post("/post", post);
+router.post("/approval/:token", approval)
 
 module.exports = router;
