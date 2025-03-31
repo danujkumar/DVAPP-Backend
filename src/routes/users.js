812d9286a21c2907fd3458fda7360ce0c35f2c61
6015Anurag <134766075+6015Anurag@users.noreply.github.com>
2025-03-31 11:25:13 +0530

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { getimage, post, approval } = require("../controllers/service");
-//The end point as given in the problem statement
+const router = express.Router();
 
 router.get("/getimage/:token", getimage);
 router.post("/post", post);
