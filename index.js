db2cea65287422ca183e87317dd0a466f75fe613
6015Anurag <134766075+6015Anurag@users.noreply.github.com>
2025-03-31 10:49:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ connectToDb((err) => {
     }
 });
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
